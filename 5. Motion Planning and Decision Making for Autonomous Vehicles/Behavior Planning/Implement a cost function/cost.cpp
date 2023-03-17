710793c4ad5e7a8d10fc4b53a0a20002071d177e
Casca Kwok <casca.kwok@gmail.com>
2023-03-16 20:35:16 -0700

    Update cost.cpp

--- 5. Motion Planning and Decision Making for Autonomous Vehicles/Behavior Planning/Implement a cost function/cost.cpp
+++ 5. Motion Planning and Decision Making for Autonomous Vehicles/Behavior Planning/Implement a cost function/cost.cpp
@@ -1,16 +1,10 @@
-#include "cost.h"
-#include <cmath>
-
 double goal_distance_cost(int goal_lane, int intended_lane, int final_lane, 
                           double distance_to_goal) {
   // The cost increases with both the distance of intended lane from the goal
   //   and the distance of the final lane from the goal. The cost of being out 
   //   of the goal lane also becomes larger as the vehicle approaches the goal.
-    
-  /**
-   * TODO: Replace cost = 0 with an appropriate cost function.
-   */
-  double cost = 0;
-    
+  int delta_d = 2.0 * goal_lane - intended_lane - final_lane;
+  double cost = 1 - exp(-(std::abs(delta_d) / distance_to_goal));
+  
   return cost;
 }
